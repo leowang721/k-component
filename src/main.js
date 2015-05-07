90198a2f41e8184256bebea783fc9577f07f6ee7
wangkemiao <wangkemiao@baidu.com>
2015-05-07 11:29:24 +0800

    bump 0.0.1-alpha.8

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ define(function (require) {
      * @type {Object}
      */
     var component = {
-        version: '0.0.1-alpha.7'
+        version: '0.0.1-alpha.8'
     };
 
     return component;
